98f1ab90d8eca84fbc99695cf820960e85fca407
ayush katiya <ayushkatiya@gmail.com>
2023-07-26 10:20:33 +0530

    message : error_Occuring_eleminated

--- Client/src/components/FilterSection.jsx
+++ Client/src/components/FilterSection.jsx
@@ -1,25 +1,71 @@
 import { motion } from "framer-motion";
 import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import { IoFastFood } from "../assets/icons";
+import { IoFastFood, IoSearch } from "../assets/icons";
 import { useSelector } from "react-redux";
 import { staggerFadeInOut } from "../animations";
 import { statuses } from "../utils/styles";
 import SliderCard from "./SliderCard";
-import { Spinner } from "../components";
+import { Spinner, Pagination } from "../components";
+
+// Rest of the code remains the same
+
 
 const FilterSection = () => {
   const [selectedCategory, setSelectedCategory] = useState("fruits");
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [showAll, setShowAll] = useState(false);
+  const productsPerPage = 6;
+
   const products = useSelector((state) => state.products);
   const isLoading = useSelector((state) => state.isLoading);
-
   useEffect(() => {
-    setSelectedCategory("fruits"); // Reset the selected category when the component mounts
+    setSelectedCategory("fruits");
+    setCurrentPage(1);
+    setShowAll(false);
   }, []);
 
-  const filteredProducts = selectedCategory
-    ? products.filter((data) => data.product_category === selectedCategory)
-    : products;
+  useEffect(() => {
+    if (showAll) {
+      setSelectedCategory(null);
+    }
+  }, [showAll]);
+  const handleSortOrder = () => {
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  };
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+  if (isLoading || products === null) {
+    return <Spinner />;
+  }
+
+  const filteredProducts = products.filter(
+    (data) =>
+      ((showAll && !selectedCategory) || data.product_category === selectedCategory) &&
+      data.product_name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+  const sortedProducts = filteredProducts.slice().sort((a, b) => {
+    const priceA = parseFloat(a.product_price);
+    const priceB = parseFloat(b.product_price);
+    return sortOrder === "asc" ? priceA - priceB : priceB - priceA;
+  });
+  const totalProducts = sortedProducts.length;
+  const lastProductIndex = currentPage * productsPerPage;
+  const firstProductIndex = lastProductIndex - productsPerPage;
+  const currentProducts = sortedProducts.slice(firstProductIndex, lastProductIndex);
+  const handleShowAll = () => {
+    setShowAll(!showAll);
+    setCurrentPage(1);
+  }
+  const totalPages = Math.ceil(totalProducts / productsPerPage);
+
+  const handlePaginationClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
 
   return (
     <motion.div className="w-full flex flex-col items-start justify-start">
@@ -30,7 +76,7 @@ const FilterSection = () => {
         </div>
         {selectedCategory && (
           <button
-            onClick={() => setSelectedCategory(null)}
+            onClick={handleShowAll}
             className="px-4 py-2 rounded-md bg-red-500 text-white font-semibold shadow-md hover:bg-red-600 focus:outline-none"
           >
             Show All
@@ -45,42 +91,35 @@ const FilterSection = () => {
             data={data}
             selectedCategory={selectedCategory}
             setSelectedCategory={setSelectedCategory}
+            index={index}
           />
         ))}
       </div>
 
       {isLoading ? (
-        <div className="w-full flex items-center justify-evenly flex-wrap gap-4 mt-12">
-          {[...Array(6)].map((_, index) => (
-            <SkeletonLoader key={index} />
-          ))}
-        </div>
-      ) : filteredProducts.length > 0 ? (
-        <div className="w-full flex items-center justify-evenly flex-wrap gap-4 mt-12">
-          {filteredProducts.map((data, index) => (
-            <SliderCard key={index} data={data} index={index} />
-          ))}
-        </div>
+        <Spinner />
       ) : (
-        <p className="text-primary font-semibold mt-4">No products in this category.</p>
+        <div className="w-full flex items-center justify-evenly flex-wrap gap-4 mt-12">
+          {filteredProducts.length > 0 ? (
+            filteredProducts.map((data, index) => (
+              <SliderCard key={index} data={data} index={index} />
+            ))
+          ) : (
+            <p className="text-primary font-semibold">No products in this category.</p>
+          )}
+        </div>
       )}
     </motion.div>
   );
 };
 
-const SkeletonLoader = () => {
-  return (
-    <div className="w-28 h-40 bg-gray-300 animate-pulse rounded-md"></div>
-  );
-};
-
-const FilterCard = ({ data, selectedCategory, setSelectedCategory }) => {
+export const FilterCard = ({ data, index, selectedCategory, setSelectedCategory }) => {
   const isActive = data.category === selectedCategory;
 
   return (
     <motion.div
-      key={data.id}
-      {...staggerFadeInOut(data.id)}
+      key={index}
+      {...staggerFadeInOut(index)}
       onClick={() => setSelectedCategory(data.category)}
       className={`group w-28 min-w-[128px] cursor-pointer rounded-md py-6 ${
         isActive ? "bg-red-500" : "bg-primary"
@@ -106,16 +145,6 @@ const FilterCard = ({ data, selectedCategory, setSelectedCategory }) => {
       </p>
     </motion.div>
   );
-};
-
-FilterCard.propTypes = {
-  data: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    category: PropTypes.string.isRequired,
-  }).isRequired,
-  selectedCategory: PropTypes.string,
-  setSelectedCategory: PropTypes.func.isRequired,
 };
 
 export default FilterSection;
